ad6e4e2278de45e532222957dc89040378f7aee6
namddh <ddhnam21@clc.fitus.edu.vn>
2024-07-12 10:43:31 +0700

    Added children prop to the Breadcrumb component.

--- src/components/ui/breadcrumb.tsx
+++ src/components/ui/breadcrumb.tsx
@@ -9,7 +9,11 @@ const Breadcrumb = React.forwardRef<
   React.ComponentPropsWithoutRef<"nav"> & {
     separator?: React.ReactNode
   }
->(({ ...props }, ref) => <nav ref={ref} aria-label="breadcrumb" {...props} />)
+>(({ ...props }, ref) => (
+  <nav ref={ref} aria-label="breadcrumb" {...props}>
+    {props.children}
+  </nav>
+))
 Breadcrumb.displayName = "Breadcrumb"
 
 const BreadcrumbList = React.forwardRef<
