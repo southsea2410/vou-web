1cb705ef9191030ee51d50c62941df4d730a33f6
namddh <ddhnam21@clc.fitus.edu.vn>
2024-09-02 09:54:41 +0700

    Add CORS

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -13,6 +14,27 @@ const nextConfig = {
         hostname: "vouawsbucket.s3.ap-southeast-1.amazonaws.com",
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Set your origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
   },
 };
 
